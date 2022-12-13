17a2c326fe1fea4e8525f0c2b187a366658596e5
monir <monir.bu18@gmail.com>
2022-12-13 17:50:18 +0600

    class 2 code

---
+++ DST02/DST02/MyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DST02
+{
+    internal class MyClass
+    {
+        static void main()
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+}
---
+++ DST02/DST02/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace DST02
+{
+    class Program
+    {
+        static void Main() 
+        {
+            //int a = 10;
+            //bool b = false;
+            //float f = 10.5f;
+            //double d = 5.021;
+            //decimal de = 5.555m;
+
+            //a = Convert.ToInt16(d);
+
+            //Console.WriteLine("Hello World "+ d.ToString().GetType());
+            //Console.WriteLine("Hello World {0}", a);
+
+            //object x = 10.06;
+            //dynamic y = 20.5;
+
+            //Console.WriteLine(x.GetType());
+            //Console.WriteLine(y.GetType());
+
+            //int zz = 10, yy = 20; 
+
+            //var z = 10;
+            //Console.WriteLine(zz.GetType());
+            //Console.WriteLine(z.GetType());
+
+            //Guid guid= Guid.NewGuid();  
+            //Console.WriteLine(guid.ToString());
+            //Console.WriteLine(guid);
+
+            //DateTime dd = DateTime.Now;
+            //Console.WriteLine(dd);
+
+            //Guid obj = Guid.NewGuid();
+            //Console.WriteLine("New Guid is " + obj.ToString());
+            //Console.ReadLine();
+            
+            //double age = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine(age);
+
+            ConsoleKeyInfo d = Console.ReadKey();
+            Console.WriteLine(d.Key);
+        }
+    }
+}
